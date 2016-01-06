d488d12bfe90edcb5f619459fe7d84d7ee376c2c
Dale Bustad <dale@divmain.com>
2016-01-06 00:10:15 -0800

    Add tests for setDeepValue.

--- spec/utils.spec.js
+++ spec/utils.spec.js
@@ -1,5 +1,43 @@
-// import utils from "src/utils";
+import * as utils from "src/utils";
 
 describe("utils", () => {
+  describe("setDeepValue", () => {
+    it("replaces an existing value", () => {
+      const obj = {
+        deep: {
+          prop: "hello"
+        }
+      };
+      utils.setDeepValue(obj, ["deep", "prop"], "works fine");
 
+      expect(obj).to.have.deep.property("deep.prop", "works fine");
+    });
+
+    it("adds a previously non-existant property", () => {
+      const obj = {
+        deep: {}
+      };
+      utils.setDeepValue(obj, ["deep", "prop"], "works fine");
+
+      expect(obj).to.have.deep.property("deep.prop", "works fine");
+    });
+
+    it("adds non-existant key-path nodes as objects", () => {
+      const obj = {};
+      utils.setDeepValue(obj, ["deep", "prop"], "works fine");
+      utils.setDeepValue(obj, ["very", "very", "deep", "prop"], "works also");
+
+      expect(obj).to.have.deep.property("deep.prop", "works fine");
+      expect(obj).to.have.deep.property("very.very.deep.prop", "works also");
+    });
+
+    it("can traverse arrays", () => {
+      const obj = {
+        deep: [{}]
+      };
+      utils.setDeepValue(obj, ["deep", "0", "prop"], "should work");
+
+      expect(obj).to.have.deep.property("deep[0].prop", "should work");
+    });
+  });
 });
