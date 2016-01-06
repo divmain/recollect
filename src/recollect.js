d620a220833654277c7ae5a89f26662b9054cf07
Dale Bustad <dale@divmain.com>
2016-01-06 03:24:42 -0800

    Fallback for queries passed to getIxdbQuery.  Queries are sometimes optional.

--- src/recollect.js
+++ src/recollect.js
@@ -18,7 +18,7 @@ export * as Errors from "./errors";
  * @return {Object}       Query-literal with modified keypaths.
  */
 function getIxdbQuery (query) {
-  return Object.keys(query).reduce((modified, keyPath) => {
+  return Object.keys(query || {}).reduce((modified, keyPath) => {
     const escapedKey = keyPath.indexOf("$meta.") === 0 ? keyPath : `$data.${keyPath}`;
     modified[escapedKey] = query[keyPath];
     return modified;
